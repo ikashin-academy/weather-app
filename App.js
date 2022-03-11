d829bdfbac045e3a33e628ce176860d684943782
Ivan Kashin <i.kashin@liga-team.ru>
2022-03-11 13:39:59 +0300

    Прокидывает condition в Weather

--- App.js
+++ App.js
@@ -13,10 +13,16 @@ export default class App extends React.Component {
   };
 
   getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    this.setState({ isLoading: false, temp: data.main.temp });
+
+    this.setState({ isLoading: false, temp, condition: weather[0].main });
 
     console.log({ data });
   };
@@ -36,7 +42,15 @@ export default class App extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp } = this.state;
-    return <>{isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />}</>;
+    const { isLoading, temp, condition } = this.state;
+    return (
+      <>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Weather condition={condition} temp={Math.round(temp)} />
+        )}
+      </>
+    );
   }
 }
--- Weather.js
+++ Weather.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const Weather = ({ temp }) => {
+const Weather = ({ temp, condition }) => {
   return (
     <View style={styles.container}>
       <Text>{temp}</Text>
@@ -20,6 +20,25 @@ const Weather = ({ temp }) => {
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Dust",
+    "Ash",
+    "Squall",
+    "Tornado",
+    "Clear",
+    "Clouds",
+  ]).isRequired,
 };
 
 export default Weather;
